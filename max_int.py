8045003d2022fffae21deb744e7ebcb366ef7fc0
Lukas Brazaitis <lukasb19@ru.is>
2019-09-05 09:43:47 +0000

    added some comments

--- max_int.py
+++ max_int.py
@@ -1,9 +1,11 @@
 #program that finds max int of inputs until negative int is input
+#after you insert a number, if its higher than the current number it becomes max_int
+#max int starts at 0.
 num_int = int(input("Input a number: "))    # Do not change this line
 max_int = 0
 # Fill in the missing code
 while num_int >= 0:
-    num_int = int(input("Enter a number: "))
+    num_int = int(input("Input a number: "))
     if num_int > max_int:
         max_int = num_int
 

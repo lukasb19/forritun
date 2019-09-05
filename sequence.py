23de1c265353c25abeeea2c0d07a82d58488c5c1
Lukas Brazaitis <lukasb19@ru.is>
2019-09-05 11:00:07 +0000

    made sure that you get correct outputs if you input 1,2 or 3

--- sequence.py
+++ sequence.py
@@ -4,14 +4,25 @@
 #That way you get the next number
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(1)
+    print(2)
+elif n == 3:
+    print(1)
+    print(2)
+    print(3)
 current_int = 3
 previous_int = 2
 last_int = 1
 n -= 3             #withdraw 3 from n because I already set the first 3 numbers in the sequence
 
-print(last_int) 
-print(previous_int) 
-print(current_int)
+if n > 0:
+    print(last_int) 
+    print(previous_int) 
+    print(current_int)
 for i in range(n):
     #I add last 2 ints to the current int, make that the current int
     #then i make the last int be previous int, and previous int be current int before it was changed

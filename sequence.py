b952ad59612cee8e58da9671cb8c3f9c5fcebd4e
Lukas Brazaitis <lukasb19@ru.is>
2019-09-05 10:06:01 +0000

    Finished the code

--- sequence.py
+++ sequence.py
@@ -4,3 +4,14 @@
 #That way you get the next number
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+current_int = 3
+previous_int = 2
+last_int = 1
+n -= 3             #withdraw 3 from n because I already set the first 3 numbers in the sequence
+
+print(last_int, previous_int, current_int, end=" ")
+for i in range(n):
+    #I add last 2 ints to the current int, make that the current int
+    #then i make the last int be previous int, and previous int be current int before it was changed
+    current_int, previous_int, last_int = current_int + previous_int + last_int, current_int, previous_int
+    print(current_int, end=" ")
